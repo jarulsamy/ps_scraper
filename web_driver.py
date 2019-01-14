af89aa8e075d6669ab01ed658e175ba3804ec983
JoshuaA9088 <joshua.gf.arul@gmail.com>
2019-01-14 11:09:29 -0700

    Added page wait / exception Removed personal data

--- web_driver.py
+++ web_driver.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import time
 
 def download_htmls(username=None, password=None):
@@ -16,54 +20,54 @@ def download_htmls(username=None, password=None):
     driver = webdriver.Chrome()
     website = driver.get("https://ps.acsd1.org/public/")
 
-    time.sleep(2)
-
     # Find Username, password, and button
     uname = driver.find_element_by_name("account")
     pword = driver.find_element_by_name("pw")
     button = driver.find_element_by_id("btn-enter-sign-in")
 
-    time.sleep(0.5)
-
     # Substitite with Username and Password
     uname.send_keys(username)
     pword.send_keys(password)
-
-    time.sleep(0.3)
-
+    
+    # Submit
     button.click()
-
-    time.sleep(1)
 
     # Switch to new URL
     url = "https://ps.acsd1.org/guardian/home.html"
     driver.get(url)
 
+    # Find the write hyperlinks. 
     class_pages = driver.find_elements_by_class_name("bold")
     for i in class_pages:
         if str(i.text)[0] in possible_grades:
             grade_pages.append(i)
 
+    # Iterate through all class pages and download HTMLs
     for i in grade_pages:
         f = open("Downloads/" + str(grade_pages.index(i)) + ".html", "wb")
 
         # Open and switch to new tab
         i.send_keys(Keys.CONTROL + Keys.RETURN)
         driver.switch_to.window(driver.window_handles[-1])
+        
+        # Wait till element loads, prevents incomplete page downloads
+        try:
+            myElem = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'ng-binding')))
+        # Timeout of 10 seconds
+        except TimeoutException:
+            print("Loading took too much time!")
+            print("Likely an internet issue!")
+            exit(0)
 
         # Save the source to file
-        time.sleep(5)
         f.write(driver.page_source.encode('utf-8').strip())
         driver.close()
 
         # Cleanup, switch back to main tab
-        time.sleep(1)
         driver.switch_to.window(driver.window_handles[0])
         f.close()
-        time.sleep(1)
 
     # Kill main window
-    time.sleep(.1)
     driver.close()
 
     print("Done grabbing HTMLs")
