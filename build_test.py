7be5ed0802527252409bb38a424d702ac488ac9c
JoshuaA9088 <joshua.gf.arul@gmail.com>
2019-01-16 13:07:07 -0700

    Updated environment vars

--- build_test.py
+++ build_test.py
@@ -2,6 +2,8 @@ import web_driver
 import process_html
 import os
 
+print("Username: ", os.getenv("test_user"))
+
 web_driver.download_htmls(username=os.getenv("test_user"), password=os.getenv("test_password"))
 process_html.gen_excel()
 process_html.cleanup(everything=True)
