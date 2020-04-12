eb30e64bf7954a907f69c3a71eea32dee48827d7
Joshua Arulsamy <joshua.gf.arul@gmail.com>
2020-04-12 03:41:47 -0600

    Added request based auth system.

--- src/web_driver.py
+++ src/web_driver.py
@@ -1,84 +1,87 @@
-import sys
-import time
+import base64
+import hashlib
+import hmac
+import re
 
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+import requests
 
 
-def download_htmls(username=None, password=None, url=None):
-    if url is None:
-        url = "https://ps.acsd1.org/guardian/home.html"
+class Powerschool(object):
+    def __init__(self, uname, pword, url=None):
+        super().__init__()
 
-    # Possible grades to find correct hyperlinks
-    possible_grades = ("A", "B", "C", "D", "F")
-    grade_pages = []
-    html_data = []
+        if url is None:
+            url = "https://ps.acsd1.org/"
 
-    # Headless options
-    options = FirefoxOptions()
-    options.add_argument("--headless")
-    driver = webdriver.Firefox(firefox_options=options)
+        self.uname = uname
+        self.pword = pword
+        self.url = url
+        self.session = requests.session()
+        self.auth()
 
-    # Switch to new URL
-    driver.get(url)
+    def _getAuthData(self):
+        r = self.session.get(self.url)
 
-    # Find Username, password, and button
-    uname = driver.find_element_by_name("account")
-    pword = driver.find_element_by_name("pw")
-    button = driver.find_element_by_id("btn-enter-sign-in")
+        if r.status_code != requests.codes.ok:
+            raise Exception("Unable to retrieve authentication tokens from PS server.")
 
-    # Substitite with Username and Password
-    uname.send_keys(username)
-    pword.send_keys(password)
+        self.auth_data = {}
 
-    # Submit
-    button.click()
+        pstoken = re.search(
+            r'<input type="hidden" name="pstoken" value="(.*?)" \/>', r.text, re.S
+        )
+        self.auth_data["pstoken"] = pstoken.groups()[0]
 
-    try:
-        driver.find_element_by_class_name("feedback-alert")
-        print("Invalid Username or Password")
-        sys.exit(1)
-    except NoSuchElementException:
-        pass
-        # print("Successfully Logged in...")
+        contextData = re.search(
+            r'<input type="hidden" name="contextData" value="(.*?)" \/>', r.text, re.S
+        )
+        self.auth_data["contextData"] = contextData.groups()[0]
 
-    # Find the right hyperlinks.
-    class_pages = driver.find_elements_by_class_name("bold")
-    for i in class_pages:
-        if str(i.text)[0] in possible_grades:
-            grade_pages.append(i)
+        if "<input type=hidden name=ldappassword value=''>" in r.text:
+            self.auth_data["ldap"] = True
+        else:
+            self.auth_data["ldap"] = False
 
-    # Iterate through all class pages and save HTML
-    for i in grade_pages:
-        # Open and switch to new tab
-        i.send_keys(Keys.CONTROL + Keys.RETURN)
-        time.sleep(1)
-        driver.switch_to.window(driver.window_handles[1])
+    def auth(self):
+        authdata = self._getAuthData()
+        dbpw = hmac.new(
+            authdata["contextData"].encode("ascii"),
+            self.pword.lower().encode("ascii"),
+            hashlib.md5,
+        ).hexdigest()
+        pw = hmac.new(
+            authdata["contextData"].encode("ascii"),
+            base64.b64encode(hashlib.md5(self.pword.encode("ascii")).digest()).replace(
+                b"=", b""
+            ),
+            hashlib.md5,
+        ).hexdigest()
 
-        # Wait till element loads, prevents incomplete page downloads
-        try:
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "ng-binding"))
+        fields = {
+            "pstoken": authdata["pstoken"],
+            "contextData": authdata["contextData"],
+            "dbpw": dbpw,
+            "serviceName": "PS Parent Portal",
+            "pcasServerUrl": "/",
+            "credentialType": "User Id and Password Credential",
+            "account": self.uname,
+            "pw": pw,
+        }
+
+        if authdata["ldap"]:
+            fields["ldappassword"] = self.pword
+
+        r = self.session.post(self.url + "guardian/home.html", data=fields)
+
+        if u"Grades and Attendance" not in r.text:
+            pserror = re.search(
+                r'<div class="feedback-alert">(.*?)<\/div>', r.text, re.S
             )
-        # Timeout of 10 seconds
-        except TimeoutException:
-            print("Loading took too long!")
-            sys.exit(0)
 
-        # Save the source to memory
-        html_data.append(driver.page_source.encode("utf-8").strip())
-
-        # Cleanup, switch back to main tab
-        driver.switch_to.window(driver.window_handles[0])
-
-    # Kill main window
-    driver.close()
-
-    # print("Done grabbing HTMLs")
-    return html_data
+            if pserror:
+                raise Exception(pserror.groups()[0])
+            else:
+                raise Exception("Unable to login to PS server.")
+        else:
+            print(r.text)
+            # return user(self, r.text)
